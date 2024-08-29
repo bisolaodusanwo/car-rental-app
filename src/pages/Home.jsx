2a2e30d651ec77458b1c1a71122f253c15d3f568
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-08-29 13:29:01 +0100

    chore: Add Tailwind CSS configuration and import styles in main.jsx and Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import BrandSection from '../components/BrandSection'
+import Hero from '../components/Hero'
+
+const Home = () => {
+  return (
+    <div>
+        <Hero />
+        <BrandSection/>
+    </div>
+  )
+}
+
+export default Home
